9c0de7929cac7930605e22d0dd6253fd957f0e43
wenjun <wenjunice@gmail.com>
2016-10-04 20:40:54 -0400

    modify port

--- webapp.py
+++ webapp.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from random import randrange
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,4 +8,6 @@ def hello():
     return str(randrange(1,7))
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0', port=port)
+    
