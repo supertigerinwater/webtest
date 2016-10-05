3b6175fdef6757d1842b3091d5a74fa18aff46be
wenjun <wenjunice@gmail.com>
2016-10-04 20:23:09 -0400

    modified

--- webapp.py
+++ webapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return str(randrange(1,7))
+    return "The dicing number is: ", str(randrange(1,7))
 
 if __name__ == "__main__":
     app.run()
